0b9370f6a87e6ae3c66004e6b5cedd9d30c92e2d
NataliaNalina <mvnatalia@mail.ru>
2025-05-17 23:53:40 +0300

    initialization

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Home work if.");
+        System.out.println("Задача 1. ");
+    }
+}
