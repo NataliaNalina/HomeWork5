99f3a8e5259a66002293082b8de645c7c48f3fe6
NataliaNalina <mvnatalia@mail.ru>
2025-05-18 19:49:46 +0300

    HW5

--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,11 @@ public class Main {
         if (clientOS == 1) {
             if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную веррию приложения для Android по ссылке");
-        } else {
+            } else {
                 System.out.println("Установите веррию приложения для Android по ссылке");
-        }
+            }
         } else {
-             if (clientDeviceYear < 2015) {
+            if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную веррию приложения для iOS по ссылке");
             } else {
                 System.out.println("Установите веррию приложения для iOS по ссылке");
@@ -28,32 +28,25 @@ public class Main {
         }
         System.out.println("Задача 3. Определение високосного года");
         System.out.println("Введите год: ");
-        int Year = in.nextInt();
-        if (Year>=1584 && Year % 4 == 0) {
-            if ((Year % 100 == 0 && Year % 400 == 0) || (Year % 100 != 0)) {
-                System.out.println("Год " + Year + " является високосным");
-            } else {
-                System.out.println("Год " + Year +" не является високосным");
-            }
+        int year = in.nextInt();
+        if (year>=1584 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+                System.out.println("Год " + year + " является високосным");
         } else {
-            System.out.println("Год " + Year +" не является високосным");
+                System.out.println("Год " + year +" не является високосным");
         }
+
         System.out.println("Задача 4. Введите расстояние до вашего дома: ");
-        System.out.println("Введите год: ");
         int deliveryDistance = in.nextInt();
-        if (deliveryDistance < 60) {
-            if (deliveryDistance < 20) {
-                System.out.println("Доставка займет 1 день.");
-            } else {
+        if (deliveryDistance <=20) {
+            System.out.println("Доставка займет 1 день.");
+        } else  if (deliveryDistance > 20 && deliveryDistance <= 60)  {
                 System.out.println("Доставка займет 2 дня.");
-            }
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Доставка займет 3 дня.");
         } else {
-            if (deliveryDistance > 100) {
                 System.out.println("К сожаленю, доставки до Вас нет.");
-            } else {
-                System.out.println("Доставка займет 3 дня.");
-            }
         }
+
         System.out.println("Задача 5. Введите номер месяца года: ");
         int monthYear = in.nextInt();
         switch (monthYear) {
